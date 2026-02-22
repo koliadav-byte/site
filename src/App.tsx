470a344b734c8e4a69df85c5d98bc8dadba29052
koliadav-byte <koliada.v@gmail.com>
2026-02-22 23:46:12 +0300

    v5

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Header from './sections/Header';
 import Hero from './sections/Hero';
 import MostImportant from './sections/MostImportant';
-import CategoryGrid from './sections/CategoryGrid';
+import WhatWeDo from './sections/WhatWeDo';
+import ProductsAccess from './sections/ProductsAccess';
+import LatestResearch from './sections/LatestResearch';
+import Rubrics from './sections/Rubrics';
+import CadenceCalendar from './sections/CadenceCalendar';
+import SubscribeContact from './sections/SubscribeContact';
 import Sidebar from './sections/Sidebar';
 import Footer from './sections/Footer';
 import './App.css';
@@ -43,12 +48,32 @@ function App() {
       <Header />
       <main className="pt-[120px]">
         <Hero />
+
         <div className="max-w-[1400px] mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex gap-8">
             <div className="flex-1">
+              {/* 1) Navigator as the intellectual center */}
               <MostImportant />
-              <CategoryGrid />
+
+              {/* 2) What we do */}
+              <WhatWeDo />
+
+              {/* 3) Products / Access */}
+              <ProductsAccess />
+
+              {/* 4) Latest research */}
+              <LatestResearch />
+
+              {/* 5) Rubrics */}
+              <Rubrics />
+
+              {/* 6) Cadence / Calendar */}
+              <CadenceCalendar />
+
+              {/* 7) Subscribe / Contact */}
+              <SubscribeContact />
             </div>
+
             <Sidebar />
           </div>
         </div>
